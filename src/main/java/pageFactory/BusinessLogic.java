e212a2c5ffed9484a73c0d76c6fc300f7f01d52c
akakiy <ivachkevich63@gmail.com>
2022-05-14 23:54:01 +0300

    write scenario

--- src/main/java/pageFactory/BusinessLogic.java
+++ src/main/java/pageFactory/BusinessLogic.java
@@ -11,18 +11,6 @@ import java.util.List;
 public class BusinessLogic {
     PropertiesReader properties = new PropertiesReader();
 
-    public void addToBucketExpensiveElement(SearchPage searchPage, String brand) throws InterruptedException {
-        searchPage.clickCheckBoxMsi(brand);
-        Thread.sleep(2000);
-        searchPage.clickPopUp();
-        searchPage.clickPopUpExpensive();
-        Thread.sleep(2000);
-        List<WebElement> listAddToBucket =  searchPage.getListAddToBucket();
-        Thread.sleep(2000);
-        listAddToBucket.get(0).click();
-        searchPage.clickGoToBucket();
-    }
-
     public void addToBucketExpensiveElementNoThreadSleep(SearchPage searchPage, String brand) {
         searchPage.clickCheckBoxMsi(brand);
         searchPage.clickPopUpByText(" От дорогих к дешевым ");
--- src/test/resources/script.feature
+++ src/test/resources/script.feature
@@ -1,8 +1,13 @@
 Feature: Test select expensive
   As user select expensive product and add to card
-  Scenario Outline: Test select
-    Given User input '<product>'
 
+  Scenario Outline: Test select
+    Given User input '<product>' in search field
+    And User check '<brand>'
+    And User select '<select>'
+    When User add first element to bucket
+    Then The price in the cart is higher than the '<price>'
 
     Examples:
-    | product |
+      | product | brand | price | select |
+      | ноутбук | MSI   | 5000  | " От дорогих к дешевым " |
